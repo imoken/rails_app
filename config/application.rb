4b69255e97852e306eb362f4b4ff0281fd52a68d
imototakeshi <imototakeshi@gmail.com>
2014-08-23 09:42:49 +0900

    niocle-app

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,9 @@ module FirstApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # For Heroku
+    config.assets.initialize_on_precompile = false
+
   end
 end
-# For Heroku
-#config.assets.initialize_on_precompile = false
